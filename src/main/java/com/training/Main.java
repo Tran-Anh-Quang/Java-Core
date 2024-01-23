33c8017fbc98fcce3b49029d5a1d49076b3f7b7d
dinos <quangta1@newwave.vn>
2024-01-23 17:52:25 +0700

    Completed exercise for Vector & Queue

--- src/main/java/com/training/Main.java
+++ src/main/java/com/training/Main.java
@@ -7,10 +7,13 @@ import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
+        runArrayListExercise();
+        runLinkedListExercise();
+        runVectorExercise();
+        runQueueExercise();
+    }
 
-        System.out.println("============================================");
-        System.out.println("Bài tập ArrayList:");
-        System.out.println("============================================");
+    private static void runArrayListExercise() {
 
         List<String> list = new java.util.ArrayList<>();
 
@@ -20,49 +23,45 @@ public class Main {
         list.add("4");
         list.add("5");
 
-//        for (String i: list){
-//            System.out.println(i);
-//        }
 
         System.out.println("get list = " + list);
 
 //        System.out.println("get value at index [2] = " + list.get(2));
 //        System.out.println("get value at index [1] = " + list.get(1));
-
+//
 //        System.out.println("remove value at index [2] = " + list.remove(2));
 //        System.out.println("get list after remove at index [2] = " + list);
-
+//
 //        System.out.println("index of the number 2 is [" + list.indexOf("2") + "]");
-
-
+//
+//
 //        list.add(2, "8");
 //        System.out.println("after add new number at index [2] = " + list);
-
+//
 //        list.add(5, "9");
 //        System.out.println("after add new number at index [5] = " + list);
 //
 //        list.clear();
 //        System.out.println("after clear list = " + list);
 //        System.out.println("check empty = " + list.isEmpty());
-
+//
 //        System.out.println("size of list = " + list.size());
-
-
-        List<String> list1 = new java.util.ArrayList<>();
-
-        list1.add("2");
-        list1.add("3");
-        list1.add("4");
-
+//
+//
+//        List<String> list1 = new java.util.ArrayList<>();
+//
+//        list1.add("2");
+//        list1.add("3");
+//        list1.add("4");
+//
 //        list1.addAll(list);
+//
+//        System.out.println("get list 1 = " + list1);
+//
+//        System.out.println("after retain list 1 = " + list.retainAll(list1));
+    }
 
-        System.out.println("get list 1 = " + list1);
-
-//        System.out.println("after retain list 1 = " + list.retainAll(list1)); // retain = chỉ giữ lại những thằng mà list 1 có, còn lại delete
-
-        System.out.println("============================================");
-        System.out.println("Bài tập LinkedList:");
-        System.out.println("============================================");
+    private static void runLinkedListExercise() {
 
         LinkedList<String> linkedList = new LinkedList<>();
 
@@ -76,33 +75,32 @@ public class Main {
 
 //        linkedList.add(5, "Sáu");
 //        System.out.println("After add new value at index [5] = " + linkedList);
-
+//
 //        linkedList.addFirst("Không");
 //        System.out.println("after add first a new value = " + linkedList);
-
+//
 //        linkedList.addLast("Sáu");
 //        System.out.println("after add last a new value = " + linkedList);
-
+//
 //        System.out.println("get first = " + linkedList.getFirst());
 //        System.out.println("get last = " + linkedList.getLast());
-
+//
 //        System.out.println("size of linked list = " + linkedList.size());
-
-//        if (linkedList.contains("Một")){
+//
+//        if (linkedList.contains("Một")) {
 //            System.out.println("Có chứa nha");
-//        }else{
+//        } else {
 //            System.out.println("Không có nha");
 //        }
-
+//
 //        System.out.println(linkedList.remove()); // auto delete thằng đầu tiên nếu không truyền index
 //        System.out.println(linkedList.remove(4));
-
+//
 //        System.out.println(linkedList.indexOf("Ba"));
 //        System.out.println(linkedList.lastIndexOf("Bốn"));
+    }
 
-        System.out.println("============================================");
-        System.out.println("Bài tập Vector:");
-        System.out.println("============================================");
+    private static void runVectorExercise() {
 
         Vector<String> vector = new Vector<>();
 
@@ -116,14 +114,14 @@ public class Main {
 
 //        vector.add(5, "Mười một");
 //        System.out.println("after add new value at index [5] = " + vector);
-
+//
 //        vector.clear();
 //        System.out.println("after clear = " + vector);
-
+//
 //        System.out.println(vector.contains("Mười"));
-
+//
 //        System.out.println(vector.indexOf("Tám"));
-
+//
 //        System.out.println("get index [2] = " + vector.get(2));
 
 
@@ -134,15 +132,14 @@ public class Main {
 
 //        vector1.addAll(vector);
 //        System.out.println(vector1);
-
+//
 //        vector1.addElement("Mười");
 //        System.out.println(vector1);
-
+//
 //        System.out.println("after retain vector 1 = " + vector.retainAll(vector1));
+    }
 
-        System.out.println("============================================");
-        System.out.println("Bài tập Vector:");
-        System.out.println("============================================");
+    private static void runQueueExercise() {
 
         Queue<String> queue = new LinkedList<>();
 
@@ -153,18 +150,19 @@ public class Main {
         queue.add("6");
         queue.add("7");
 
-        System.out.println(queue);
-
+//        System.out.println(queue);
+//
 //        System.out.println(queue.offer("1"));
 //        System.out.println(queue.remove());
-
+//
 //        queue.clear();
 //        System.out.println(queue.poll());
-
+//
 //        System.out.println(queue.element());
-
+//
 //        queue.clear();
 //        System.out.println(queue.peek());
-
     }
+
 }
+
